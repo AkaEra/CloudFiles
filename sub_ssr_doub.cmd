c59078e25792a8d09a308565764e335cc6dbc002
Aka Era <akaera@foxmail.com>
2018-09-12 15:04:38 +0800

    update sub_ssr_doub

--- sub_ssr_doub.cmd
+++ sub_ssr_doub.cmd
@@ -1,4 +1,4 @@
 certutil -f -encode sub_ssr_doub.txt sub_ssr_doub_tmp.txt
-findstr /v /c:- sub_ssr_doub_tmp.txt > sub_ssr_doub.html
+findstr /v /c:- sub_ssr_doub_tmp.txt > sub_ssr_doub_b64.txt
 del sub_ssr_doub_tmp.txt
 pause
